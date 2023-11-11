a3deee1d1bf8da24fb4b78da114ddb0b752fde32
pras75299 <singh.prashantking@gmail.com>
2023-11-11 18:31:07 +0530

    login api complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,48 +5,85 @@ import jwt from "jsonwebtoken";
 class UserController {
   static userRegistration = async (req, res) => {
     const { name, email, password, password_confirmation, tc } = req.body;
-    const user = await UserModel.findOne({ email });
-    if (user) {
-      res.send({ status: "failed", message: "Email already exists" });
-    } else {
+    try {
+      const user = await UserModel.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .send({ status: "failed", message: "Email already exists" });
+      }
       if (name && email && password && password_confirmation && tc) {
         if (password === password_confirmation) {
-          try {
-            const salt = await bcrypt.genSalt(10);
-            const hashPassword = await bcrypt.hash(password, salt);
-            const data = new UserModel({
-              name: name,
-              email: email,
-              password: hashPassword,
-              tc: tc,
-            });
-            await data.save();
-            res.json({ data });
-          } catch (error) {
-            console.log(error);
-          }
+          const salt = await bcrypt.genSalt(10);
+          const hashPassword = await bcrypt.hash(password, salt);
+          const data = new UserModel({
+            name,
+            email,
+            password,
+            tc,
+          });
+          await data.save();
+          res.status(201).json({ status: "success", data });
         } else {
-          res.send({
+          res.status(400).send({
             status: "failed",
             message: "Password and confirm password doesn't match",
           });
         }
       } else {
-        res.send({ status: "failed", message: "All fields are required" });
+        res
+          .status(400)
+          .send({ status: "failed", message: "All fields are required" });
       }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ status: "failed", message: "Server Error" });
     }
   };
 
   static getUsers = async (req, res) => {
     try {
       const users = await UserModel.find({});
-      return res.status(200).json({
-        count: users.length,
-        data: users,
-      });
+      return res
+        .status(200)
+        .json({ status: "success", count: users.length, users });
     } catch (error) {
-      console.log(error.message);
-      res.status(500).send({ message: error.message });
+      console.error(error.message);
+      res.status(500).send({ status: "failed", message: "Server Error" });
+    }
+  };
+
+  static userLogin = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      if (email && password) {
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+          return res.status(401).send({
+            status: "failed",
+            message: "Email not registered",
+          });
+        }
+        console.log("User found:", user);
+        if (password === user.password) {
+          res.send({
+            status: "success",
+            message: "Login Success",
+          });
+        } else {
+          res.status(401).send({
+            status: "failed",
+            message: "Password is not valid",
+          });
+        }
+      } else {
+        res
+          .status(400)
+          .send({ status: "failed", message: "All Fields are Required" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ status: "failed", message: "Server Error" });
     }
   };
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ import UserController from "../controllers/userController.js";
 
 //public routes
 router.post("/register", UserController.userRegistration);
+router.post("/login", UserController.userLogin);
 router.get("/getUsers", UserController.getUsers);
 //private routes
 
